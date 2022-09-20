5f4ed4d8c79d0d7e51d5c67d35a7bf0f971d2324
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-09-20 16:53:49 +0500

    task13 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int Digit(int number)
+{
+    int digit = number.ToString()[2]- '0';
+    return digit;
+}
+if (number>=100)
+{
+    int a = Digit(number);
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
