b659597cf9fb919834d5be74dc2a1cdd68dd4eb3
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-09-20 10:51:10 +0500

    task 10 complited

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+//принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int insertThreedigitnumber()
+{
+    Console.WriteLine("Введите трехзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int Seconddigit(int number)
+{
+    int dec = (number % 100)/10;
+        return dec;
+}
+int a = Seconddigit(insertThreedigitnumber());
+Console.WriteLine(a);
