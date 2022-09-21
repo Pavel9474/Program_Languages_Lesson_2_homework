38ff81c008c6d785ecc196847abf997696415b85
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-09-21 11:54:58 +0500

    addtask14 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно
+//одновременно 7 и 23.
+
+void multiplysevenandtwentythree()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number%7==0 & number%23==0)
+    {
+       Console.WriteLine("Кратно"); 
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
+multiplysevenandtwentythree();
