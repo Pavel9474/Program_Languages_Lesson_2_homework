5fa075b6eb4680f3d57bf97d692339b6159d904e
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-09-22 11:07:01 +0500

    additional task 16 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли
+//одно число квадратом другого.
+void square()
+{
+    Console.WriteLine("Введите 2 числа:");
+    int firstnumber = Convert.ToInt32(Console.ReadLine());
+    int secondnumber = Convert.ToInt32(Console.ReadLine());
+    if (firstnumber/secondnumber==secondnumber)
+    {
+        Console.WriteLine("Первое число явлется квадратом второго");
+    }
+    else if (secondnumber/firstnumber==firstnumber)
+    {
+        Console.WriteLine("Второе число является квадратом первого");
+    }
+    else
+    {
+        Console.WriteLine("Ни одно из чисел не является квадратом другого");
+    }
+}
+square();
